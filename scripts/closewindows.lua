ba158a2e3606463168c4b598da947add8a79506d
JustinFreitas <justin.s.freitas@me.com>
2024-02-12 21:36:43 -0700

    v1.1 - add timer to options

--- scripts/closewindows.lua
+++ scripts/closewindows.lua
@@ -1,6 +1,7 @@
 CLOSEWINDOWS_KEEP_CT_OPEN = "CLOSEWINDOWS_KEEP_CT_OPEN";
 CLOSEWINDOWS_KEEP_IMAGES_OPEN = "CLOSEWINDOWS_KEEP_IMAGES_OPEN";
 CLOSEWINDOWS_KEEP_PS_OPEN = "CLOSEWINDOWS_KEEP_PS_OPEN";
+CLOSEWINDOWS_KEEP_TIMER_OPEN = "CLOSEWINDOWS_KEEP_TIMER_OPEN";
 IS_FGC = false;
 OFF = "off";
 ON = "on";
@@ -31,6 +32,8 @@ function onInit()
     OptionsManager.registerOption2(CLOSEWINDOWS_KEEP_IMAGES_OPEN, true, option_header, "option_label_CLOSEWINDOWS_KEEP_IMAGES_OPEN", option_entry_cycler,
     { labels = option_val_on, values = ON, baselabel = option_val_off, baseval = OFF, default = OFF });
     OptionsManager.registerOption2(CLOSEWINDOWS_KEEP_PS_OPEN, true, option_header, "option_label_CLOSEWINDOWS_KEEP_PS_OPEN", option_entry_cycler,
+    { labels = option_val_on, values = ON, baselabel = option_val_off, baseval = OFF, default = OFF });
+    OptionsManager.registerOption2(CLOSEWINDOWS_KEEP_TIMER_OPEN, true, option_header, "option_label_CLOSEWINDOWS_KEEP_TIMER_OPEN", option_entry_cycler,
     { labels = option_val_on, values = ON, baselabel = option_val_off, baseval = OFF, default = OFF });
     Comm.registerSlashHandler("cw", closeWindows);
     Comm.registerSlashHandler("closewindows", closeWindows);
@@ -85,8 +88,9 @@ function closeWindow(t, i)
         and t[i].close ~= nil then
         if keepCtOpen(t, i)
             or keepImagesOpen(t, i)
-            or keepPsOpen(t, i) then
-            return true;
+            or keepPsOpen(t, i)
+            or keepTimerOpen(t, i) then
+                return true;
         end
 
         t[i].close();
@@ -112,6 +116,11 @@ end
 function keepPsOpen(t, i)
     local keepPsOpen = OptionsManager.isOption(CLOSEWINDOWS_KEEP_PS_OPEN, ON);
     return keepPsOpen and (t[i].getClass() == "partysheet_host" or t[i].getClass() == "partysheet_client");
+end
+
+function keepTimerOpen(t, i)
+    local keepTimerOpen = OptionsManager.isOption(CLOSEWINDOWS_KEEP_TIMER_OPEN, ON);
+    return keepTimerOpen and t[i].getClass() == "timerwindow";
 end
 
 function onWindowOpened(window)
